db0eecbfbb0397ff279d027ea0fa746460ef8ec9
kannan-baskar <202239835+kannanbaskar123@users.noreply.github.com>
2025-09-24 13:00:29 +0530

    baseline image set

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
       pageShots: {
       pages: [ { path: '/', name: 'landing' } ],
-      baseUrl: 'http://172.17.0.1:3000',
+      baseUrl: 'http://127.0.0.1:5500/index.html#',
     },
       lostPixelProjectId: 'cmfm3yb5t0pxm3rwpl73ahw4i',
       apiKey: process.env.LOST_PIXEL_API_KEY,
